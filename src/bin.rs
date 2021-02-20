4c03c87dc7ab410569cfc5befb2d9cfcfa606ec0
MorganKF <mfudge92@gmail.com>
2021-02-20 20:17:18 -0330

    Added connection error handling and more comments

--- src/bin.rs
+++ src/bin.rs
@@ -4,6 +4,7 @@ use crate::both::*;
 use std::net::TcpListener;
 use std::thread::spawn;
 use tungstenite::server::accept;
+use tungstenite::Message;
 
 pub fn main() {
     println!("Starting PAC server!");
@@ -11,19 +12,31 @@ pub fn main() {
     // Start server on localhost
     let server = TcpListener::bind("127.0.0.1:8080").unwrap();
 
+    // Loop over incoming connections
     for stream in server.incoming() {
+        // Spawn a thread for each connection
+        // Todo: Connections should be moved to a async thread pool instead of using system threads
         spawn(move || {
+            // Accept connection and start reading incoming messages
             let mut websocket = accept(stream.unwrap()).unwrap();
             loop {
-                // Todo: Unexpected connection close event
-                let msg = websocket.read_message().unwrap();
+                let next = websocket.read_message();
+                if let (Ok(ref msg)) = next {
+                    if msg.is_text() {
+                        // Deserialize message into a PacEvent
+                        let event: PacEvent = serde_json::from_str(&msg.to_string()).unwrap();
 
-                if msg.is_text() {
-                    let event: PacEvent = serde_json::from_str(&msg.to_string()).unwrap();
-                    println!("{:?}", event);
-                    websocket
-                        .write_message(serde_json::to_string(&PacEvent::start()).unwrap().into())
-                        .unwrap();
+                        websocket
+                            .write_message(
+                                serde_json::to_string(&PacEvent::start()).unwrap().into(),
+                            )
+                            .unwrap();
+                    }
+                }
+
+                // Errors will be primarily triggered by a ConnectionClose error so we will break the loop and join the thread
+                if let (Err(e)) = next {
+                    break;
                 }
             }
         });
