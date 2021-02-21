5bfca3c1d0816ffcf3d037c5d8b20d7e58064776
MorganKF <mfudge92@gmail.com>
2021-02-20 20:30:28 -0330

    Added simple server state

--- src/bin.rs
+++ src/bin.rs
@@ -1,7 +1,10 @@
 mod both;
+mod server;
 
 use crate::both::*;
+use crate::server::State;
 use std::net::TcpListener;
+use std::sync::{Arc, Mutex};
 use std::thread::spawn;
 use tungstenite::server::accept;
 use tungstenite::Message;
@@ -12,13 +15,26 @@ pub fn main() {
     // Start server on localhost
     let server = TcpListener::bind("127.0.0.1:8080").unwrap();
 
+    // Initialize server state
+    let state = Arc::new(Mutex::new(State::new()));
+
     // Loop over incoming connections
     for stream in server.incoming() {
+        // Clone state to move into thread
+        let state_ref = state.clone();
+
         // Spawn a thread for each connection
         // Todo: Connections should be moved to a async thread pool instead of using system threads
         spawn(move || {
-            // Accept connection and start reading incoming messages
+            // Accept connection
             let mut websocket = accept(stream.unwrap()).unwrap();
+
+            // Increment node count in server state
+            {
+                state_ref.lock().unwrap().nodes += 1;
+            }
+
+            // Continuously try to read messages from the connection
             loop {
                 let next = websocket.read_message();
                 if let (Ok(ref msg)) = next {
@@ -36,6 +52,7 @@ pub fn main() {
 
                 // Errors will be primarily triggered by a ConnectionClose error so we will break the loop and join the thread
                 if let (Err(e)) = next {
+                    state_ref.lock().unwrap().nodes -= 1;
                     break;
                 }
             }
---
+++ src/server/state.rs
@@ -0,0 +1,9 @@
+pub struct State {
+    pub nodes: usize,
+}
+
+impl State {
+    pub fn new() -> Self {
+        Self { nodes: 0 }
+    }
+}
